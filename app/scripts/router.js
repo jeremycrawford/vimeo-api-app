bc3e3512684e8d4d01c66e2842d80504394f7ee8
Jeremy Crawford <jcrawford.design@gmail.com>
2014-02-26 23:13:10 -0500

    Made changes to the router.js

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,6 +1,20 @@
 var MainRouter = Backbone.Router.extend({
   routes: {
-    "shops" : "showShops",
-    
+    "poler/:videos" : "polerVideo"
   }
 })
+
+initialize: function(){
+    this.videos = new VideosCollection;
+    this.videos.on('add', function(item){
+      new VideoView({model: item})
+    })
+  },
+ 
+  showShops: function(videos){
+    if (keyword) {
+      this.items.url = "https://api.etsy.com/v2/listings/active.js?keywords="+ keyword +"&includes=Images&api_key=kr9rjq7dc9c24jv6fccq2hus&callback=?"
+    }
+    this.items.fetch();
+  }
+});
